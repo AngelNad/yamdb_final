fc09dade84157fd5a466e8e80f6b0ad45204f26f
AngelNad <angelnad@yandex.ru>
2022-02-08 21:00:52 +0300

    static script

--- api_yamdb/myscript.sh
+++ api_yamdb/myscript.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python manage.py makemigrations --no-input && \
-python manage.py migrate --no-input && \
+python manage.py migrate --noinput && \
 python manage.py collectstatic --no-input && \
 gunicorn api_yamdb.wsgi:application --bind 0:8000
